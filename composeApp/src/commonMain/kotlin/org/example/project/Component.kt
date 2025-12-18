64f4b24a50e58de2c90758f4ceada6869d1d2580
Yur4Arkhipov <yura4rkhipov@yandex.ru>
2025-12-18 16:09:55 +0300

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/Component.kt
@@ -0,0 +1,58 @@
+package org.example.project
+
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LazyColumnAnimation() {
+    var list by remember {
+        mutableStateOf(listOf("Пушкин", "Есенин", "Маяковский", "Толстой", "Достоевский", "Чехов"))
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(items = list, key = { it }) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(fraction = 0.8f)
+                    .background(Color.LightGray)
+                    .padding(24.dp)
+                    .animateItem(
+                        fadeInSpec = tween(durationMillis = 1200),
+                        placementSpec = tween(durationMillis = 1200),
+                        fadeOutSpec = tween(durationMillis = 1200)
+                    ),
+                text = it
+            )
+        }
+
+        item {
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 0.5f),
+                onClick = { list = list.shuffled() }
+            ) {
+                Text(text = "Перемешать")
+            }
+        }
+    }
+}
